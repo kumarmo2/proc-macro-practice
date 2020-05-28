327d4dacebd96fd88cb699848fecd8a23ee3bedf
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-29 02:13:17 +0530

    builder first test done.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,8 @@
 extern crate proc_macro;
-
 use proc_macro::TokenStream;
-
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(_: TokenStream) -> TokenStream {
+    // Prefix o_ means original
+    // Prefix b_ means builder
+    TokenStream::new()
 }
