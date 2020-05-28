77b029ce9fe2018b4eb01b04908936789a28cf66
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-29 02:21:31 +0530

    builder test 2 done.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,20 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(Builder)]
-pub fn derive(_: TokenStream) -> TokenStream {
+pub fn derive(input: TokenStream) -> TokenStream {
     // Prefix o_ means original
     // Prefix b_ means builder
-    TokenStream::new()
+
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let o_name = &ast.ident;
+
+    let t = quote! {
+        impl #o_name {
+            pub fn builder() {}
+        }
+    };
+    t.into()
 }
